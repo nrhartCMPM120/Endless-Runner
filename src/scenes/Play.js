10c1566e7112bde512c68f10f3a641ce17da27fc
lkaldes <lkaldes@ucsc.edu>
2022-04-24 13:23:02 -0700

    Added Time Score

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -15,13 +15,28 @@ class Play extends Phaser.Scene {
         this.runner = new Runner(this, game.config.width/2 - 200, borderUISize*10, 'runner', 0).setOrigin(0, 0);
         this.ghost01 = new GhostCharge(this, game.config.width + borderUISize*6, borderUISize*10, 'ghost', 0).setOrigin(0, 0);
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-
-
+        let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#000000',
+            align: 'center',
+            padding: {
+            top: 5,
+            bottom: 5,
+            },
+            fixedWidth: 100
+        }
+        this.tim = 0;
+        this.gameover = false;
+        this.timer = this.add.text(game.config.width/2, borderUISize + borderPadding*2, this.tim / 1000, scoreConfig).setOrigin(0.5);
         //added spike
         this.spike = new Spike(this, game.config.width+300 + borderUISize*6, borderUISize*11, 'spike', 0).setOrigin(0, 0);
     }
 
     update(){
+        if (!this.gameover) this.tim += 10;
+        this.timer.text = parseInt(this.tim / 1000);
         this.ghost01.update();
         this.runner.update();
         this.spike.update();
