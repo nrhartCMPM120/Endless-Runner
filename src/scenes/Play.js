cf085da256a411463a18c85b9e4ebafc04e631ef
Leon Chen <lchen166@ucsc.edu>
2022-04-24 04:00:06 -0700

    added spikes and runner

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,7 +5,8 @@ class Play extends Phaser.Scene {
 
     preload(){
         this.load.image('ghost', './assets/spaceship.png');
-        this.load.image('runner', './assets/rocket.png');
+        this.load.image('runner', './assets/runner.png');
+        this.load.image('spike', './assets/spike.png');
     }
 
     create(){
@@ -14,10 +15,15 @@ class Play extends Phaser.Scene {
         this.runner = new Runner(this, game.config.width/2 - 200, borderUISize*10, 'runner', 0).setOrigin(0, 0);
         this.ghost01 = new GhostCharge(this, game.config.width + borderUISize*6, borderUISize*10, 'ghost', 0).setOrigin(0, 0);
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+
+
+        //added spike
+        this.spike = new Spike(this, game.config.width+300 + borderUISize*6, borderUISize*11, 'spike', 0).setOrigin(0, 0);
     }
 
     update(){
         this.ghost01.update();
         this.runner.update();
+        this.spike.update();
     }
 }
