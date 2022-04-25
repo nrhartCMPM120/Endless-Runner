dc13e3d7185b99b67dbd2d8c7d22a5efe6fb99bb
Leon Chen <lchen166@ucsc.edu>
2022-04-24 19:26:09 -0700

    added bad background

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,9 +7,17 @@ class Play extends Phaser.Scene {
         this.load.image('ghost', './assets/spaceship.png');
         this.load.image('runner', './assets/runner.png');
         this.load.image('spike', './assets/spike.png');
+        this.load.image('background', './assets/background.png');
+        this.load.image('ghostb','./assets/ghost.png');
     }
 
     create(){
+
+        //added background
+        this.background = this.add.tileSprite(0, 0, 640, 480, 'background').setOrigin(0, 0);
+
+        this.ghostb = this.add.tileSprite(0, 0, 640, 480, 'ghostb').setOrigin(0, 0);
+
         //this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize*3, game.config.width, borderUISize*3, 0xFFFFFF).setOrigin(0, 0);
         this.runner = new Runner(this, game.config.width/2 - 200, borderUISize*10, 'runner', 0).setOrigin(0, 0);
@@ -32,13 +40,20 @@ class Play extends Phaser.Scene {
         this.timer = this.add.text(game.config.width/2, borderUISize + borderPadding*2, this.tim / 1000, scoreConfig).setOrigin(0.5);
         //added spike
         this.spike = new Spike(this, game.config.width+300 + borderUISize*6, borderUISize*11, 'spike', 0).setOrigin(0, 0);
+
     }
 
     update(){
+
+        //move background
+        this.background.tilePositionX += 4; 
+        this.ghostb.tilePositionX += 2;
+
         if (!this.gameover) this.tim += 10;
         this.timer.text = parseInt(this.tim / 1000);
         this.ghost01.update();
         this.runner.update();
         this.spike.update();
+
     }
 }
