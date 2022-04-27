9e4c8679ce12f0da5e4cab8bf5f73d13139a23bb
Leon Chen <lchen166@ucsc.edu>
2022-04-26 22:10:30 -0700

    added backgrounds

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,9 +7,12 @@ class Play extends Phaser.Scene {
         this.load.image('ghost', './assets/spaceship.png');
         this.load.image('runner', './assets/runner.png');
         this.load.image('spike', './assets/spike.png');
-        this.load.image('background', './assets/background.png');
+        this.load.image('background', './assets/nightground.png');
         this.load.image('ghostb','./assets/ghost.png');
         this.load.image('ground','./assets/ground.png');
+        this.load.image('clouds', './assets/clouds.png');
+        this.load.image('talltrees', './assets/talltrees.png');
+        
     }
 
     create(){
@@ -18,6 +21,10 @@ class Play extends Phaser.Scene {
         this.background = this.add.tileSprite(0, 0, 640, 480, 'background').setOrigin(0, 0);
 
         this.ghostb = this.add.tileSprite(0, 0, 640, 480, 'ghostb').setOrigin(0, 0);
+
+        this.clouds = this.add.tileSprite(0, 0, 640, 480, 'clouds').setOrigin(0, 0);
+
+        this.talltrees = this.add.tileSprite(0, 0, 640, 480, 'talltrees').setOrigin(0, 0);
 
         // floor
         //this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
@@ -60,8 +67,10 @@ class Play extends Phaser.Scene {
         }
 
         //move background
-        this.background.tilePositionX += 4; 
-        this.ghostb.tilePositionX += 2;
+        //this.background.tilePositionX += 4; 
+        //this.ghostb.tilePositionX += 2;
+        this.talltrees.tilePositionX += 3;
+        this.clouds.tilePositionX += 2;
 
         if (!this.gameover){ 
             if (Phaser.Input.Keyboard.JustDown(keyW) && this.runner.body.touching.down) {
